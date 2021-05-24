3198b82fa1cd67c659260e3d15329a1dfa908df9
pablo beltran <pablobeltran219@gmail.com>
2021-05-24 13:40:35 -0700

    agregue registrar usuario y valodacion para login

--- apme_2020/urls.py
+++ apme_2020/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import login, configuration, viewer
+from .views import login, configuration, viewer, registrar_usuario
 
 urlpatterns = [
-    path('login/', login),
+    path('login/', login, name='login'),
+    path('registrar_usuario/', registrar_usuario, name='registrar_usuario'),
+    
     path('configuration/', configuration),
-    path('viewer/', viewer),
+    path('viewer/', viewer, name='viewer'),
 ]
--- apme_2020/views.py
+++ apme_2020/views.py
@@ -1,8 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Login
 
 # Create your views here.
 def login(request):
-    return render(request, 'login.html',{})
+    if request.method == 'POST':
+        try:
+            name = request.POST['user']
+            password = request.POST['password']
+            registred_user = Login.objects.get(name=name, password=password)
+            if registred_user:
+                return render(request, 'configuration.html', {})
+        except:
+            return render(request, 'login.html', {'error':'Sesion no encontrada!'})
+    else:
+        return render(request, 'login.html', {})
+
+def registrar_usuario(request):
+    if request.method == 'POST':
+        try:
+            name = request.POST['user']
+            password = request.POST['password']
+            email = request.POST['email']
+            usuario_registrado = Login.objects.get(name=name)
+        except:
+            nuevo_usuario = Login(name=name, password=password, email=email)
+            nuevo_usuario.save()
+            return render(request, 'registrar_usuario.html', {'mensaje':'Registro Exitoso!'})
+    else:
+        return render(request, 'registrar_usuario.html', {})
+
+
 
 def configuration(request):
     return render(request, 'configuration.html',{})
