7ea9fc5471298d4500b7bb097dd797995d9257a0
pablo beltran <pablobeltran219@gmail.com>
2021-05-24 12:18:13 -0700

    agregue fecha de actializacion en contexto para el index

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from datetime import date
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {})
+    last_update = date.today()
+    return render(request, 'index.html', {'last_update':last_update})
 
 def desktop_gui(request):
     return render(request, 'desktop_gui.html',{})
