a12d9b41f35c025829ff8826955c81e8189a6f40
pablo beltran <pablobeltran219@gmail.com>
2021-05-25 21:03:09 -0700

    agregue templates para configuration

--- apme_2020/views.py
+++ apme_2020/views.py
@@ -9,11 +9,13 @@ def login(request):
             password = request.POST['password']
             registred_user = Login.objects.get(name=name, password=password)
             if registred_user:
-                return render(request, 'configuration.html', {})
+                return redirect(configuration)
+                #return render(request, 'configuration.html', {})
         except:
             return render(request, 'login.html', {'error':'Sesion no encontrada!'})
     else:
         return render(request, 'login.html', {})
+    
 
 def registrar_usuario(request):
     if request.method == 'POST':
@@ -33,7 +35,15 @@ def registrar_usuario(request):
 
 
 def configuration(request):
-    return render(request, 'configuration.html',{})
+    if request.method == 'POST':
+        return render(request, 'configuration.html')
+    else:
+        return render(request, 'login.html')
+
+    
 
 def viewer(request):
-    return render(request, 'viewer.html',{})
+    if request.method == 'POST':
+        return render(request, 'viewer.html')
+    else:
+        return render(request, 'login.html')
