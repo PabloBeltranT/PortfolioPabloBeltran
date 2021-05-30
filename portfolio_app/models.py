19aa7b96202e6f55d43832bbe69ac9162358b088
pablo beltran <pablobeltran219@gmail.com>
2021-05-30 16:00:19 -0700

    agregue contador para visitas

--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class Visits(models.Model):
+    from_to = models.CharField(max_length=50)
--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from datetime import date
+from .models import Visits
 
 # Create your views here.
 def index(request):
     last_update = date.today()
-    return render(request, 'index.html', {'last_update':last_update})
+    visit = request.META['DESKTOP_SESSION']
+    new_visit = Visits(from_to=visit)
+    new_visit.save()
+    visits = Visits.objects.all()
+    cant = 0
+    for visitant in visits:
+        cant = cant + 1
+    return render(request, 'index.html', {'last_update':last_update, 'visits':cant})
 
 def desktop_gui(request):
     return render(request, 'desktop_gui.html',{})
